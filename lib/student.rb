56d056ef30c5669901663be26ed991e1d70e4d18
Marc Poppy <poppy.marc@gmail.com>
2019-07-03 03:14:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,6 @@
 class Student
 
   def hello
-    super
     puts "How are you doing today? I'm okay, but I'm kind of tired.
     Did you watch The Walking Dead last night?
     You didn't?! Oh man, it was so crazy! What, you don't want
